80ea6420fee7a18fa0b78070079df3dc8806d3a4
Varun Negi <varunnegiweb@gmail.com>
2026-03-21 23:46:08 +0530

    Add message model

---
+++ Backend/src/models/Message.model.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+    conversation: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'Conversation',
+        required: true
+    },
+    sender : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required: true
+    },
+    receiver: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required: true
+    },
+    content: {
+        type: String,
+        trim: true
+    },
+    mediaUrl:{
+        type: String,
+    },
+    contentType:{
+        type: String,
+        enum:['image','video','text'],
+        default: 'text'
+    },
+    reactions: [
+        {
+            user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+            emoji: {type: String, required: true}
+        } 
+    ],
+    messageStatus: {
+        type: String,
+        enum: ['sent', 'delivered', 'seen'],
+        default:'send',
+    },
+},{timestamps: true});
+
+
+const Message = mongoose.model('Message', messageSchema);
+module.exports = Message;
+
