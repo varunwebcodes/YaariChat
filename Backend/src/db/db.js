bfccea0840cd99a612e33aba9719ac34a652ed42
Varun Negi <varunnegiweb@gmail.com>
2026-03-24 23:55:12 +0530

    Update db

--- Backend/src/db/db.js
+++ Backend/src/db/db.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async() => {
     try{
-        await mongoose.connect(process.env.MONGO_URI,{
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("Database Connected Successfully")
     }catch (error) {
         console.error("error connecting database", error.message)
