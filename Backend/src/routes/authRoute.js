f0d5a9ea425ceaf06a45b88fb624255b400e485f
Varun Negi <varunnegiweb@gmail.com>
2026-03-26 22:29:25 +0530

    Add update-profile route

--- Backend/src/routes/authRoute.js
+++ Backend/src/routes/authRoute.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const authController = require('../controllers/authController')
+const authController = require('../controllers/authController');
+const authMiddleware = require('../middlewares/auth.Middleware');
 
 
 const router  = express.Router();
@@ -8,5 +9,8 @@ const router  = express.Router();
 router.post('/send-otp', authController.sendOtp);
 router.post('/verify-otp', authController.verifyOtp);
 
+
+//protected route
+router.put('/update-profile' ,authMiddleware , authController.updateProfile)
 
 module.exports = router;
