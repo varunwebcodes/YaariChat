6e4acabd904560d82e5dad4d4be060a7d75627f9
Varun Negi <varunnegiweb@gmail.com>
2026-03-30 22:18:02 +0530

    feat(auth): add check-auth, users, and logout routes

--- Backend/src/routes/authRoute.js
+++ Backend/src/routes/authRoute.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const authController = require('../controllers/authController');
 const authMiddleware = require('../middlewares/auth.Middleware');
+const {multerMiddleware} = require('../db/cloudinary.db');
 
 
 const router  = express.Router();
@@ -8,9 +9,12 @@ const router  = express.Router();
 
 router.post('/send-otp', authController.sendOtp);
 router.post('/verify-otp', authController.verifyOtp);
+router.get('/logout', authController.logout)
 
 
 //protected route
 router.put('/update-profile' ,authMiddleware , authController.updateProfile)
+router.get('/check-auth', authMiddleware, authController.checkAuthenticated)
+router.get('/users' , authMiddleware, authController.getAllUsers)
 
 module.exports = router;
